b73566f461ebeb016e3c961e4cddc871110a0cdd
Aye Lecman <ayelenlecman@gmail.com>
2025-07-14 19:46:49 +0000

    correcciones al contador

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -3,27 +3,31 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-const Counter = () => {
+const DigitCounter = ({ time }) => {
+  return (
+    <h1 id="counter" className="text-light display-3 fw-bold">
+      {time}
+    </h1>
+  )
+}
 
-let num = 0;
-    setInterval(() => {
-      num++;
-      const element = document.getElementById("counter");
-      if (element) {
-        element.textContent = String(num).padStart(6, "0");
-      }
-    }, 1000);
-
-
-
-return (
-    <div className="text-center bg-dark h-100 mt-5">
-      <div className="container-fluid d-flex justify-content-center mt-5">
-        <FontAwesomeIcon icon={faClock} className="text-light fs-1 me-3 mt-4" />
-        <h1 id="counter" className="text-light display-3">000000</h1>
+const SecondsCounter = ({ seconds }) => {
+  return (
+    <div className="text-center bg-dark h-100 mt-5 p-4">
+      <div className="container-fluid d-flex justify-content-center align-items-center gap-5">
+        <FontAwesomeIcon icon={faClock} className="text-light fs-1 me-3" />
+        <DigitCounter time={Math.floor((seconds % 1000000) / 100000)} />
+        <DigitCounter time={Math.floor((seconds % 100000) / 10000)} />
+        <DigitCounter time={Math.floor((seconds % 10000) / 1000)} />
+        <DigitCounter time={Math.floor((seconds % 1000) / 100)} />
+        <DigitCounter time={Math.floor((seconds % 100) / 10)} />
+        <DigitCounter time={Math.floor((seconds % 10) / 1)} />
       </div>
     </div>
-  );
+  )
+
+
+
 };
 
-export default Counter;
+export default SecondsCounter;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -11,8 +11,19 @@ import '../styles/index.css'
 // components
 import SecondsCounter from "./components/SecondsCounter.jsx";
 
+let start = 0
+
+const interval = setInterval(() => {
+  start++;
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <SecondsCounter seconds={start} />
+    </React.StrictMode>,
+  )
+}, 1000)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SecondsCounter/>
-  </React.StrictMode>,
-)
+    <SecondsCounter seconds={start} />
+  </React.StrictMode>)
